fa803602edb3480559e188403a373b4625177389
fadelafuente <fdelafuente242@gmail.com>
2023-07-24 16:51:13 -0500

    add password encoding and check email is valid and does not exists before saving user

--- src/main/java/com/example/demo/appuser/AppUserService.java
+++ src/main/java/com/example/demo/appuser/AppUserService.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class AppUserService implements UserDetailsService {
     private final AppUserRepository repository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final static String USER_NOT_FOUND_MSG =
             "user with email %s not found";
 
@@ -19,5 +21,17 @@ public class AppUserService implements UserDetailsService {
         return repository.findByEmail(email)
                 .orElseThrow(() ->
                         new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+    }
+
+    public String signUpUser(AppUser appUser) {
+        boolean userExists = repository.findByEmail(appUser.getEmail()).isPresent();
+        if(userExists) {
+            throw new IllegalStateException("email already taken");
+        }
+        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+        appUser.setPassword(encodedPassword);
+        repository.save(appUser);
+        // TODO: send confirmation token
+        return "works";
     }
 }
